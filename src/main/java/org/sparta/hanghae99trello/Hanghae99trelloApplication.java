30ecddd35eed983f2d39ff0e845e4b78b56fe544
KimDabomi <cdabomi@nate.com>
2024-01-12 10:13:35 +0900

    init: 초기 세팅

---
+++ src/main/java/org/sparta/hanghae99trello/Hanghae99trelloApplication.java
@@ -0,0 +1,13 @@
+package org.sparta.hanghae99trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghae99trelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hanghae99trelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparta/hanghae99trello/Hanghae99trelloApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.hanghae99trello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hanghae99trelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
