54c44ae13a8b2178659ddbb7489330871c8ee13e
KimDabomi <cdabomi@nate.com>
2024-01-12 16:00:55 +0900

    refactor: 오타 수정

--- src/main/java/org/sparta/hanghae99trello/Hanghae99TrelloApplication.java
+++ src/main/java/org/sparta/hanghae99trello/Hanghae99TrelloApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Hanghae99trelloApplication {
+public class Hanghae99TrelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Hanghae99trelloApplication.class, args);
+        SpringApplication.run(Hanghae99TrelloApplication.class, args);
     }
 
 }
