bd7446de146c8349d15cc3cf225900a216eb2936
KimDabomi <cdabomi@nate.com>
2024-01-13 16:39:06 +0900

    feat: scripts 수정

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,9 +1,10 @@
 cd ./build/libs/
+java -jar hanghae99Trello-0.0.1-SNAPSHOT.jar
 
-latest_jar=$(ls -t *.jar | head -n 1)
-
-if [ -n "$latest_jar" ]; then
-  java -jar "$latest_jar"
-else
-  echo "No JAR file found in ./build/libs/"
-fi
+#latest_jar=$(ls -t *.jar | head -n 1)
+#
+#if [ -n "$latest_jar" ]; then
+#  java -jar "$latest_jar"
+#else
+#  echo "No JAR file found in ./build/libs/"
+#fi
