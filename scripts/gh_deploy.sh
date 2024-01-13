dff3314383bd635c5058c77b89375d30536ded9d
KimDabomi <cdabomi@nate.com>
2024-01-13 16:12:13 +0900

    feat: scripts 수정

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,15 +1,2 @@
-#!/bin/bash
-
-# Change to the build/libs directory
 cd ./build/libs/
-
-# Find the most recently modified JAR file
-latest_jar=$(ls -t *.jar | head -n 1)
-
-# Check if a JAR file was found
-if [ -n "$latest_jar" ]; then
-  # Run the JAR file
-  java -jar "$latest_jar"
-else
-  echo "No JAR file found in ./build/libs/"
-fi
+java -jar hanghae99Trello-0.0.1-SNAPSHOT.jar
--- src/main/java/org/sparta/hanghae99trello/controller/HelloController.java
+++ src/main/java/org/sparta/hanghae99trello/controller/HelloController.java
@@ -1,16 +1,19 @@
 package org.sparta.hanghae99trello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hey")
-    public String hello() {
+    public String hey() {
         return "hey 제발 쫌!!!!!!!!!!!!!!!!!";
     }
-    @GetMapping("/hello")
-    public String hellohello(){
+
+    @PostMapping("/hello")
+    public String hello(){
         return "야 왜안돼";
     }
 }
