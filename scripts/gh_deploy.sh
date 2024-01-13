5b6b1c41c139167e63078930dfee7db1fd7726e9
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2024-01-13 19:33:44 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -3,8 +3,7 @@ cd ../build/libs/
 latest_jar=$(ls -t *.jar | head -n 1)
 
 if [ -n "$latest_jar" ]; then
-  chmod +x "$latest_jar"
-  nohup java -jar "$latest_jar" > output.log 2>&1 &
+  java -jar "$latest_jar"
 else
   echo "No JAR file found in ./build/libs/"
 fi
