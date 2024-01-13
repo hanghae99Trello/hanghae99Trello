384d4c46d41ea796cb23af5e161937141bdd911f
KimDabomi <cdabomi@nate.com>
2024-01-13 16:59:59 +0900

    fix: jwt 변수명 변경

--- src/main/java/org/sparta/hanghae99trello/jwt/JwtUtil.java
+++ src/main/java/org/sparta/hanghae99trello/jwt/JwtUtil.java
@@ -32,7 +32,7 @@ public class JwtUtil {
     private static final String BEARER_PREFIX = "Bearer ";
     private static final String JWT_LOG_HEAD = "JWT 관련 로그";
     private final long TOKEN_TIME = 60 * 60 * 1000L; // 60분
-    @Value("${jwt.secret.key}")
+    @Value("${JWT_SECRET_KEY}")
     private String secretKey;
     private Key key;
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
