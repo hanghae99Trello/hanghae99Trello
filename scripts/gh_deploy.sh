3144970629458d1c884cde6e43da51b7e996575d
Kim Dabomi <110092175+KimDabomi@users.noreply.github.com>
2024-01-13 19:59:59 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -3,7 +3,7 @@ cd ../build/libs/
 latest_jar=$(ls -t *.jar | head -n 1)
 
 if [ -n "$latest_jar" ]; then
-  java -jar "$latest_jar"
+  nohup java -jar "$latest_jar" &
 else
   echo "No JAR file found in ./build/libs/"
 fi
