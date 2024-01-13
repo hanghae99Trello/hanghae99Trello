7c71d9d0252b6f4f33fbf59fc2c6f5fa19c023f2
Kim Dabomi <110092175+KimDabomi@users.noreply.github.com>
2024-01-13 21:49:05 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,9 +1,25 @@
 cd ../build/libs/
-echo "cd" >> script.log
+
+CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
 latest_jar=$(ls -t *.jar | head -n 1)
 
 if [ -n "$latest_jar" ]; then
   java -jar "$latest_jar"
 else
-  echo "No JAR file found in ./build/libs/"
+  echo "No JAR file found in ../build/libs/"
 fi
+
+JAR_NAME=$(ls -tr ../build/libs/*SNAPSHOT.jar | tail -n 1)
+chmod +x $JAR_NAME
+nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> ../build/libs/nohup.out 2>&1 &
