cdb9b0d5f93859639e7cc6b0fd0e14dc318ca868
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2024-01-13 16:07:26 +0900

    Update HelloController.java

--- src/main/java/org/sparta/hanghae99trello/controller/HelloController.java
+++ src/main/java/org/sparta/hanghae99trello/controller/HelloController.java
@@ -9,5 +9,8 @@ public class HelloController {
     public String hello() {
         return "hey 제발 쫌!!!!!!!!!!!!!!!!!";
     }
+    @GetMapping("/hello")
+    public String hellohello(){
+        return "야 왜안돼";
 }
   
