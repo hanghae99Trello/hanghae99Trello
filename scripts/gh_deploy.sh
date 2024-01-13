39ef4b8b71328ed333de2267211763f2acf90e68
Kim Dabomi <110092175+KimDabomi@users.noreply.github.com>
2024-01-13 21:41:49 +0900

    Delete scripts directory

--- scripts/gh_deploy.sh
+++
@@ -1,9 +0,0 @@
-cd ../build/libs/
-echo "cd" >> script.log
-latest_jar=$(ls -t *.jar | head -n 1)
-
-if [ -n "$latest_jar" ]; then
-  java -jar "$latest_jar"
-else
-  echo "No JAR file found in ./build/libs/"
-fi
