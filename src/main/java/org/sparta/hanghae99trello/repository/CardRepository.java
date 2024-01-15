9e21dc522ff4bead9a3cef3d2035e379e33a0795
crescent0kt <gyungtaek0@gmail.com>
2024-01-15 17:33:03 +0900

    feat: CardRepository 추가

--- src/main/java/org/sparta/hanghae99trello/repository/CardRepository.java
+++ src/main/java/org/sparta/hanghae99trello/repository/CardRepository.java
@@ -1,4 +1,9 @@
 package org.sparta.hanghae99trello.repository;
 
-public interface CardRepository {
+import org.sparta.hanghae99trello.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card,Integer> {
 }
