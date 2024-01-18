ad78d3a856442ffc1f43df45223ded5cd84e96ec
KimDabomi <cdabomi@nate.com>
2024-01-18 21:22:16 +0900

    test: test 삭제

--- src/test/java/org/sparta/hanghae99trello/service/UserServiceTest.java
+++ src/test/java/org/sparta/hanghae99trello/service/UserServiceTest.java
@@ -20,7 +20,7 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
+//@SpringBootTest
 @ActiveProfiles("test")
 public class UserServiceTest {
 
