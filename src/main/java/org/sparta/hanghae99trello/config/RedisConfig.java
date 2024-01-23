f41f34e0d82f672d880b4980b1f40a0642edc77d
crescent0kt <gyungtaek0@gmail.com>
2024-01-23 15:29:31 +0900

    feat: deploy, compose

--- src/main/java/org/sparta/hanghae99trello/config/RedisConfig.java
+++ src/main/java/org/sparta/hanghae99trello/config/RedisConfig.java
@@ -23,7 +23,7 @@ public class RedisConfig {
 //                .setAddress("redis://127.0.0.1:6379");
 
         config.useSingleServer()
-                .setAddress("redis://cache:6380");
+                .setAddress("redis://cache:6379");
         return Redisson.create(config);
     }
 
