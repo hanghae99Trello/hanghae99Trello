14a8cb8e8f2ee3d5f81ebe29180c10ae9ae91f98
crescent0kt <gyungtaek0@gmail.com>
2024-01-23 17:47:39 +0900

    test

--- src/main/java/org/sparta/hanghae99trello/controller/HelloController.java
+++ src/main/java/org/sparta/hanghae99trello/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hey")
     public String hey() {
-        return "어디갔다가 이제왔어 왜 이제온거야 ㅜㅜㅜ ㅋㅋ";
+        return "어디갔다가 이제왔어 왜 이제온거야 ㅜㅜㅜ 이제 가도돼 ㅋㅋㅋㅋ";
     }
 }
