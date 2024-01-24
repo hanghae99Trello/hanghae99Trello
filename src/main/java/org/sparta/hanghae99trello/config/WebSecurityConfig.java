d243cc90d8d10d8dcd28e14f3ae556562e7f231b
KimDabomi <cdabomi@nate.com>
2024-01-24 23:03:32 +0900

    fix: security config

--- src/main/java/org/sparta/hanghae99trello/config/WebSecurityConfig.java
+++ src/main/java/org/sparta/hanghae99trello/config/WebSecurityConfig.java
@@ -63,8 +63,8 @@ public class WebSecurityConfig {
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .requestMatchers("/hey", "/api/join", "/join", "/static/**", "/api/user/login").permitAll()
-//                        .requestMatchers(HttpMethod.PUT).permitAll()
-//                        .requestMatchers(HttpMethod.DELETE).permitAll()
+                        .requestMatchers(HttpMethod.PUT).permitAll()
+                        .requestMatchers(HttpMethod.DELETE).permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users/boards/**").hasAnyAuthority("AUTH_USER")
                         .requestMatchers(HttpMethod.PUT, "/api/users/boards/**").hasAnyAuthority("AUTH_USER")
                         .requestMatchers(HttpMethod.GET, "/api/users/boards/**").hasAnyAuthority("AUTH_USER")
